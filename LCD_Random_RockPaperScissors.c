76fac903487e215ec3b108f8667025ebba040cc9
impelfin <impelfin@gmail.com>
2018-05-28 23:20:04 +0900

    update

--- LCD_Random_RockPaperScissors.c
+++ LCD_Random_RockPaperScissors.c
@@ -1,5 +1,5 @@
 /*
- * LCD_Random_Dice_Ex_Btn_With_Com_Melody.c
+ * LCD_Random_RockPaperScissors.c
  *
  * Created: 2018-04-16 AM 1:38:30
  * Author : MACBOOK
@@ -35,37 +35,44 @@ int main(void) {
 
 	initrand();
 	
-	unsigned char o_sw, n_sw, led, str1[16], str2[16];
+	unsigned char key, led, str1[16], str2[16];
 	int i, num = 0, com = 0;
 	
-	o_sw = PINB & (1<<PINB0);	
-	
 	while(1) {
-		n_sw = PINB & (1<<PINB0);
+		key = PINB & 0x0F;
 		led = 0xFE;
-
-		if(o_sw != 0 && n_sw == 0) {
-			num = (random() % 6) + 1;
-			com = (random() % 6) + 1;
-			for(i = 0;i < num; i++) {
-				PORTA = led;
-				led = led << 1;
-			}
+		
+		if(key == 0x08) {
+			num = 1;
+			com = (random() % 3) + 1;
+		} else if(key == 0x04) {
+			num = 2;
+			com = (random() % 3) + 1;
+		} else if(key == 0x02) {
+			num = 3;
+			com = (random() % 3) + 1;
+		} else if(key == 0x01) {
+			num = (random() % 3) + 1;
+			com = (random() % 3) + 1;
 		}
-		o_sw = n_sw;		
+		
+		if(num == 1) PORTA = 0x3F;
+		if(num == 2) PORTA = 0xE7;
+		if(num == 3) PORTA = 0xFC;
 		
 		sprintf(str1, "User=%d vs Com=%d", num, com);
-
-		if((num - com) > 0) { 
+		
+		if((num - com) == -2 || (num - com) == 1) { 
 			sprintf(str2, "User Win!!");
 			PORTD = 0x00;
 			_delay_ms(50);
 			PORTD = 0xFF;
-		} else if((num - com) < 0) {
+		} else if((num - com) == -1 || (num - com) == 2) {
 			sprintf(str2, "User Lose!!" );
 		} else {
 			sprintf(str2, "User Draw!!");			
 		}
+		
 		LCD_initialize();	// LCD 초기화 함수 call
 		LCD_position(0,0);	// LCD에 표시할 문자 위치 지정
 		LCD_STR(str1);		// LCD에 표시할 문자열 출력
